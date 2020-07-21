a6a17b6f59080125a15147732d240921cc064ab3
jiranovaa <63350637+jiranovaa@users.noreply.github.com>
2020-07-21 11:43:37 +0200

    Updated version

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+function add() {
+    let t = $('#task');
+    let i = t.val();
+    let alert = '<div class="alert-container">' + '<p class="alert-message">The TO DO field cannot be empty.</p>' + '<div class="arrow-up"></div>' + '</div>';
+
+    if (i !== '') {
+        removeAlert();
+        $('.todos-container ul').append('<li>' + i + '<div class="buttons">' + '<img src="delete-button.svg" class="delete-button">' + '<img src="tick-button.svg" class="tick-button">' + '</div>' + '</li>');
+
+        t.val('');
+    } else {
+        // alert('The TO DO field cannot be empty.');
+        $('.todo').append(alert);
+        $('#task').addClass('highlighted');
+    }
+}
+
+function removeAlert() {
+    $('.alert-container').remove();
+    $('#task').removeClass('highlighted');
+}
+
 $('#add').click(add);
 
 $('#task').keypress(function(e) {
@@ -6,19 +28,16 @@ $('#task').keypress(function(e) {
     }
     });
 
-function add() {
-    var i = $('#task').val();
-    if (i === '') {
-        alert('Add TO DO.');
-    } else {
-        $('.todos-container ul').append('<li>' + i + '<div class="buttons">' + '<img src="delete-button.svg" class="delete-button">' + '<img src="tick-button.svg" class="tick-button">' + '</div>' + '</li>');
-    }
-}
-
 $('.todos-container ul').on('click', '.delete-button', function() {
     $(this).parent('.buttons').parent('li').remove();
 });
 
 $('.todos-container ul').on('click', '.tick-button', function() {
-    $(this).replaceWith('<img src="tick-button-2.svg" class="tick-button"></img>');
+    $(this).parent('.buttons').parent('li').addClass("done");
+    $(this).replaceWith('<img src="tick-button-2.svg" class="tick-button2"></img>');
+});
+
+$('.todos-container ul').on('click', '.tick-button2', function() {
+    $(this).parent('.buttons').parent('li').removeClass("done");
+    $(this).replaceWith('<img src="tick-button.svg" class="tick-button"></img>');
 });
